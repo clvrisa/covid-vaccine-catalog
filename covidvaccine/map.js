4ba17e3cf1c3a129fcd869ef5718a13104488d29
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-07-18 22:48:19 -0700

    Add files via upload

---
+++ covidvaccine/map.js
@@ -0,0 +1,724 @@
+//https://simplemaps.com/custom/us/uVZ3pPxI
+
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 27,031 Deaths: 31"
+    },
+    AK: {
+      name: "Alaska",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 56,886 Deaths: 41"
+    },
+    FL: {
+      name: "Florida",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 1,909,209 Deaths: 147"
+    },
+    NH: {
+      name: "New Hampshire",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 76,861 Deaths: 87"
+    },
+    VT: {
+      name: "Vermont",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 16,003 Deaths: 33"
+    },
+    ME: {
+      name: "Maine",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 45,926 Deaths: 52"
+    },
+    RI: {
+      name: "Rhode Island",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 128,121 Deaths: 239"
+    },
+    NY: {
+      name: "New York",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 942,268 Deaths: 164"
+    },
+    PA: {
+      name: "Pennsylvania",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 948,643 Deaths: 190"
+    },
+    NJ: {
+      name: "New Jersey",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 812,609 Deaths: 265"
+    },
+    DE: {
+      name: "Delaware",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 88,354 Deaths: 151"
+    },
+    MD: {
+      name: "Maryland",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 388,035 Deaths: 131"
+    },
+    VA: {
+      name: "Virginia",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 586,592 Deaths: 113"
+    },
+    WV: {
+      name: "West Virginia",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 133,445 Deaths: 129"
+    },
+    OH: {
+      name: "Ohio",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 978,471 Deaths: 151"
+    },
+    IN: {
+      name: "Indiana",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 667,262 Deaths: 189"
+    },
+    IL: {
+      name: "Illinois",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 1,198,335 Deaths: 181"
+    },
+    CT: {
+      name: "Connecticut",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 285,330 Deaths: 216"
+    },
+    WI: {
+      name: "Wisconsin",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 621,654 Deaths: 122"
+    },
+    NC: {
+      name: "North Carolina",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 873,678 Deaths: 109"
+    },
+    DC: {
+      name: "District of Columbia",
+      color: "#000000",
+      hover_color: "#d3d1d1"
+    },
+    MA: {
+      name: "Massachusetts",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 568,979 Deaths: 236"
+    },
+    TN: {
+      name: "Tennessee",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 783,484 Deaths: 169"
+    },
+    AR: {
+      name: "Arkansas",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 324,818 Deaths: 176"
+    },
+    MO: {
+      name: "Missouri",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 480,913 Deaths: 132"
+    },
+    GA: {
+      name: "Georgia",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 1,023,487 Deaths: 168"
+    },
+    SC: {
+      name: "South Carolina",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 525,050 Deaths: 170"
+    },
+    KY: {
+      name: "Kentucky",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 410,709 Deaths: 107"
+    },
+    AL: {
+      name: "Alabama",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 499,819 Deaths: 206"
+    },
+    LA: {
+      name: "Louisiana",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 433,785 Deaths: 209"
+    },
+    MS: {
+      name: "Mississippi",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 297,651 Deaths: 228"
+    },
+    IA: {
+      name: "Iowa",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 339,574 Deaths: 176"
+    },
+    MN: {
+      name: "Minnesota",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 490,011 Deaths: 117"
+    },
+    OK: {
+      name: "Oklahoma",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 430,536 Deaths: 165"
+    },
+    TX: {
+      name: "Texas",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 2,686,818 Deaths: 153"
+    },
+    NM: {
+      name: "New Mexico",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 186,922 Deaths: 181"
+    },
+    KS: {
+      name: "Kansas",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 295,861 Deaths: 165"
+    },
+    NE: {
+      name: "Nebraska",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 203,027 Deaths: 109"
+    },
+    SD: {
+      name: "South Dakota",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 113,589 Deaths: 214"
+    },
+    ND: {
+      name: "North Dakota",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 100,419 Deaths: 190"
+    },
+    WY: {
+      name: "Wyoming",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 54,764 Deaths: 117"
+    },
+    MT: {
+      name: "Montana",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 100,914 Deaths: 129"
+    },
+    CO: {
+      name: "Colorado",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 436,602 Deaths: 102"
+    },
+    UT: {
+      name: "Utah",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 374,850 Deaths: 61"
+    },
+    AZ: {
+      name: "Arizona",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 826,454 Deaths: 224"
+    },
+    NV: {
+      name: "Nevada",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 296,190 Deaths: 163"
+    },
+    OR: {
+      name: "Oregon",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 157,285 Deaths: 54"
+    },
+    WA: {
+      name: "Washington",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 345,295 Deaths: 66"
+    },
+    CA: {
+      name: "California",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 3,501,394 Deaths: 136"
+    },
+    MI: {
+      name: "Michigan",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 656,072 Deaths: 166"
+    },
+    ID: {
+      name: "Idaho",
+      color: "#000000",
+      hover_color: "#d3d1d1",
+      description: "Total Cases: 172,931 Deaths: 105"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      name: "New York City",
+      lat: 40.7143528,
+      lng: -74.0059731
+    },
+    "1": {
+      name: "Anchorage",
+      lat: 61.2180556,
+      lng: -149.9002778
+    }
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  regions: {}
+};
